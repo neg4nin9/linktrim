fc0b8eb9a2ad421a59420942a732bd73ddb9cebc
EddyC <eddy.conejo@ucr.ac.cr>
2026-07-26 01:14:27 -0600

    api ping route

--- linktrim/routes/api.php
+++ linktrim/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ShortUrlController;
+use App\Models\ShortUrl;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/shorten', [ShortUrlController::class, 'shorten']);
 Route::get('/scheduler/run', [ShortUrlController::class, 'runScheduler']);
+Route::get('/ping', fn() => response()->json(['status' => 'ok', 'db' => ShortUrl::exists()]));
